ad66463bf990857b74c34ea9bd150f60723dbbfb
rahiel <rahiel@protonmail.ch>
2015-02-26 19:22:50 +0100

    exclude some files from the xpi

---
+++ build-firefox.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Run from archiveror root folder
+
+rm -rf dist/
+mkdir dist
+mkdir dist/data
+cp data/*.png dist/data/
+cp -R lib/ dist/
+cp package.json dist
+cp LICENSE dist
+
+cd dist
+cfx xpi
